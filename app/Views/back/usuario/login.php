02a68d200e88c431ad44c6b5af8ce42ae3f7e598
JAnavarro77 <navarrojuanalberto@gmail.com>
2024-07-21 19:54:28 -0300

    cambio en el formulario login

--- app/Views/back/usuario/login.php
+++ app/Views/back/usuario/login.php
@@ -1,8 +1,17 @@
+
 <form>
+  <!-- mensaje de error -->
+   <?php if(session()->getFlashdata('msg')):?>
+    <div class="alert alert-warning">
+      <?=session()->getFlashdata('msg')?>
+   </div>
+   <?php endif;?>
+   <!-- inicia el formulario del login -->
+    <form method="post" action="<?php echo base_url('enviarlogin') ?>">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Correo Electronico</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">ingresa tu corre</div>
+    <div id="emailHelp" class="form-text">iniciar sesion</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Contraseña</label>
@@ -13,4 +22,7 @@
     <label class="form-check-label" for="exampleCheck1">recordarme</label>
   </div>
   <button type="submit" class="btn btn-primary">iniciar sesión</button>
+  <br><span>¿todavia no te registraste? <a href="<?php echo base_url('/registro'); ?>">
+    Registrarse aqui </a></span>
+   
 </form>
